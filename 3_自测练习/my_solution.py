9f6080f7bfb6642fc8fb0cf23e18fcf6c3cdcf42
thiswind <thiswind@gmail.com>
2021-12-26 15:11:41 +0000

    add  部署案例

--- 3_自测练习/my_solution.py
+++ 3_自测练习/my_solution.py
@@ -1,8 +1,31 @@
+# -*- coding:utf-8 -*-
 #!/usr/bin/env python3
 
-# 待测试程序
-def solution(x):
-    return x + 2  # 为了展示测试结果，故意将x+1误写作x+2
+
+# 将两个列表是s1，s2按顺序融合为一个列表s,s为原列表
+def merge(s1, s2, s):
+    # j和i就相当于两个指向的位置，i指s1，j指s2
+    raise NotImplementedError("请实现此函数")
+
+
+# 合并排序
+def merge_sort(s):
+
+    n = len(s)
+
+    # 当只剩一个元素或没有元素时，该序列不在需要进行排序，直接返回。
+    # merge_sort函数在此处到达递归的尽头，递归从此处开始返回。
+    if n < 2:
+        return
+
+    # 拆分子序列
+    raise NotImplementedError('请补全代码块')
+
+    # 递归调用合并排序函数对子序列进行排序
+    raise NotImplementedError('请补全代码块')
+
+    # 合并
+    merge(s1, s2, s)
 
 
 if __name__ == '__main__':
--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-from my_solution import solution
+from my_solution import merge_sort
 
 
 # 测试用例
 def test_solution():
-    assert solution(8) == 9  # 判断输出结果，预期increment(8)应该为9
+    origin_list = [1, 7, 3, 5, 4, 2, 8, 9, 6]
+    sorted_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    merge_sort(origin_list)
+
+    assert sorted_list == origin_list
